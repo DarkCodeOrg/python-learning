267e2edbd47e9d0bf28c78b361e11114fde14252
DarkCodeOrg <davidclapton@protonmail.com>
2021-08-20 19:03:26 +0530

    classes and objects edited

--- classes_objects.py
+++ classes_objects.py
@@ -5,8 +5,12 @@
 # the properies defined in the class are called attributes and the functions are called methods
 
 
+
+
+
 class Human:
     def __init__(self,name,age,gender):  ## the init function is executed whenever a class is initiated   // we can use this function to pass parameters to the object when it is created  
+                                         ## this func is also called a constructor
         self.name = name
         self.age = age
         self.gender = gender
@@ -14,17 +18,46 @@ class Human:
     def greet(self):   ## self refers to the name of the object that is being creatd using this class 
         print("Hello I am " + self.name)
 
-    def say_hi(self):
+    
         print("HI EVERYBODY !!")
     
     
+class Robot:
+    def introduce_self(self):
+        print("my name is ",self.name)  ## 
+        print("my weight is " ,self.weight)
+        print("my address is ", self.addr)
 
-    
-Human1 = Human("David",32,"male")
+class Animal:
+    def __init__(self):
+        self.name = input("enter the name of the animal :")
+        self.age = int(input("enter the age of the animal :"))
+        self.habitat = input("enter the habitat of this animal :")
+        self.owner = input("enter the name of owner :")
+        self.care_taker_robo = r1
 
-print(Human1.name)
-print(Human1.age)
-print(Human1.gender)
-Human1.greet()
-Human1.say_hi()
+    def info(self):
+        print("I am a ",self.name)
+        print("my age is ",self.age)
+        print("my owner is :",self.owner)
+
+H1 = Human("David",32,"male")
+print(H1.name)
+print(H1.age)
+print(H1.gender)
+H1.greet()
+
+
+
+r1 = Robot()
+r1.name = "ROBO1"
+r1.weight = 30
+r1.addr = "USA"
+r1.introduce_self()
+
+
+
+a1 = Animal()
+a1.info()
+a1.care_taker_robo.introduce_self()
 
--- file_handling_4.py
+++ file_handling_4.py
@@ -1,1 +1,55 @@
+## updating in a binary file
+# 
+# tell() and seek() 
+#
+import pickle
+from sys import flags
+
+infile = open("trial.txt",'r')
+
+print("initially the file_pointer is at = ",infile.tell())
+print("reading 4 bytes : ",infile.read(4))
+print("now the file pointer is art :",infile.tell())
+
+## the tell function gives the current position of the file pointer
+print("now printing results of the seek function")
+
+infile.seek(10) ## default mode , puts the file pointer to the 
+                 # 10th byte from the beginning
+
+infile.seek(5,1) ## mode 1 , puts the file pointer to the 5th byte(forward direction) from
+                  # the current file pointer position
+
+infile.seek(-10,2) ## mode 2 , puts the file pointer to the 10th byte backward from the 
+                    # end of file
+
+#######################################################################
+####    UPDATING THE RECORDS OF A FILE AT A PARTICULAR POSITION     ###
+#######################################################################
+
+## read the file student_new.dat and give 10 marks bonus to students who have scored more that 550
 ## 
+
+upfile = open("student_new.dat","rb+")
+stu = {}    ## crating a empty file to store the records in the file
+found = False
+
+try:
+    while True:
+        rpos = upfile.tell()
+        stu = pickle.load(upfile)
+
+        if stu['Marks'] > 550:
+            stu['Marks'] += 10
+            upfile.seek(rpos)
+            pickle.dump(stu,upfile)
+            found = True
+
+except EOFError:
+    if found == False:
+        print("no such records found")
+    else:
+        print("record(s) have been updated")
+
+    upfile.close()
+
